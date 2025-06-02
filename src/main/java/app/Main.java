f36ae8994d75a85f602f2250b3ba3046b0f5df47
Esqueda Gonzalez Juan Pablo <22030486@itcelaya.edu.mx>
2025-06-01 23:20:11 -0600

    DiccionarioMain

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import View.LoginView;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        // Lanza la aplicación de LoginView
+        Application.launch(LoginView.class, args);
+    }
+}
